350210a73cb7b93fd07a8ff82f3f4764a27c6e9b
Bruce Garro <brucegarro@gmail.com>
2020-10-16 16:54:51 -0400

    Add title section parser and full document parser

--- preprocessing/document.py
+++ preprocessing/document.py
@@ -19,12 +19,8 @@ class Document(object):
         """
         self.root = ET.fromstring(xml_str)
 
-    def get_sentence_data(self, element):
+    def get_data_by_tags(self, element):
         data = {}
-
-        data["sentence"] = element.attrib["S"]
-        data["emotion_labals"] = get_emotion_labels(element)
-
         for node in element:
             if node.tag in TAG_TO_NAME_MAP:
                 tag = TAG_TO_NAME_MAP[node.tag][0]
@@ -32,6 +28,32 @@ class Document(object):
                 data[tag] = val
         return data
 
+    def get_sentence_data(self, element):
+        data = {}
+
+        data["text"] = element.attrib["S"]
+        data["emotion_labals"] = get_emotion_labels(element)
+        data.update(self.get_data_by_tags(element))
+
+        return data
+
     def get_all_sentence_data(self):
         for element in self.root.iter("sentence"):
             yield self.get_sentence_data(element)
+
+    def get_paragraph_data(self):
+        data = {}
+
+    def get_title_data(self, element):
+        title_data = {
+            "text": element.attrib["T"],
+        }
+        title_data.update(self.get_data_by_tags(element))
+        return title_data
+
+    def get_document_data(self):
+        title_element = self.root.find('title')
+        document_data = {
+            "title": self.get_title_data(title_element)
+        }
+        return document_data
--- test/test_document.py
+++ test/test_document.py
@@ -18,25 +18,38 @@ class TestDocument(unittest.TestCase):
 
         expected_results = [
             {
-                'sentence': '考研这个事吧，还挺费事的。',
-                'emotion_labals':
-                    {'Joy': 0.0, 'Hate': 0.0, 'Love': 0.0, 'Sorrow': 0.0, 'Anxiety': 0.5, 'Surprise': 0.0, 'Anger': 0.0, 'Expect': 0.0},
-                'length': 13,
-                'polarity': '消极'
+                "text": "考研这个事吧，还挺费事的。",
+                "emotion_labals":
+                    {"Joy": 0.0, "Hate": 0.0, "Love": 0.0, "Sorrow": 0.0, "Anxiety": 0.5, "Surprise": 0.0, "Anger": 0.0, "Expect": 0.0},
+                "length": 13,
+                "polarity": "消极"
             },
             {
-                'sentence': '一是费力，费体力，每天一大清早就得起来，特别是北邮的占座形势，尤其需要体力强一些，弱柳扶风的小女孩们千万别拿自己的娇躯开玩笑，和那些蹿得比兔子还快的男生们抢座位。',
-                'emotion_labals':
-                    {'Joy': 0.0, 'Hate': 0.2, 'Love': 0.0, 'Sorrow': 0.0, 'Anxiety': 0.4, 'Surprise': 0.0, 'Anger': 0.0, 'Expect': 0.0},
-                'length': 81,
-                'polarity': '消极'
+                "text": "一是费力，费体力，每天一大清早就得起来，特别是北邮的占座形势，尤其需要体力强一些，弱柳扶风的小女孩们千万别拿自己的娇躯开玩笑，和那些蹿得比兔子还快的男生们抢座位。",
+                "emotion_labals":
+                    {"Joy": 0.0, "Hate": 0.2, "Love": 0.0, "Sorrow": 0.0, "Anxiety": 0.4, "Surprise": 0.0, "Anger": 0.0, "Expect": 0.0},
+                "length": 81,
+                "polarity": "消极"
             },
             {
-                'sentence': '一天十多小时的复习也需要健健康康的身体来支撑，否则万一晕倒在自习室里可就不妙了。',
-                'emotion_labals': 
-                    {'Joy': 0.0, 'Hate': 0.0, 'Love': 0.0, 'Sorrow': 0.0, 'Anxiety': 0.5, 'Surprise': 0.0, 'Anger': 0.0, 'Expect': 0.0},
-                'length': 40,
-                'polarity': '消极'}
+                "text": "一天十多小时的复习也需要健健康康的身体来支撑，否则万一晕倒在自习室里可就不妙了。",
+                "emotion_labals":
+                    {"Joy": 0.0, "Hate": 0.0, "Love": 0.0, "Sorrow": 0.0, "Anxiety": 0.5, "Surprise": 0.0, "Anger": 0.0, "Expect": 0.0},
+                "length": 40,
+                "polarity": "消极"}
         ]
+
+        self.assertEqual(results, expected_results)
+
+    def test_get_document_data(self):
+        document = Document(self.xml_str)
+        results = document.get_document_data()
+        expected_results = {
+            "title": {
+                "text": "说说考研这个事",
+                "length": 7,
+                "polarity": "中性",
+            }
+        }
 
         self.assertEqual(results, expected_results)
