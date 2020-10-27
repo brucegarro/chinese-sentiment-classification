180ab591ecc3924f1386c1ac6c7d9a3cdfa2781c
Bruce Garro <brucegarro@gmail.com>
2020-10-27 16:44:42 -0400

    Add in-progress Simple-RNN model

---
+++ modeling/simple_rnn.py
@@ -0,0 +1,42 @@
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
+from tensorflow.keras.initializers import Constant
+
+from preprocessing.word_embeddings import load_pkl
+from preprocessing.document_manager import DocumentManager
+from settings.settings import EMBEDDING_MATRIX_PATH, KERAS_TOKENIZER_PATH
+
+
+class DatasetManager(object):
+    def create_dataset_from_documents(self):
+        doc_manager = DocumentManager()
+        doc_manager.cache_documents()
+
+        dataset = doc_manager.get_all_sentences_and_labels()
+        import ipdb; ipdb.set_trace()
+
+
+if __name__ == "__main__":
+    # Create Embeddings from pre-compiled embeddings matrix
+    embedding_matrix = load_pkl(EMBEDDING_MATRIX_PATH)
+    tokenizer = load_pkl(KERAS_TOKENIZER_PATH)
+
+    # Load the dataset
+    dataset_manager = DatasetManager()
+    train_x, train_y = dataset_manager.create_dataset_from_documents()
+
+    # Create Model
+    num_tokens = len(tokenizer.word_index)
+    embedding_dim = embedding_matrix[0].size
+
+    embedding_layer = Embedding(
+        input_dim=num_tokens,
+        output_dim=embedding_dim,
+        embeddings_initializer=Constant(embedding_matrix),
+        trainable=False,
+    )
+
+    model = Sequential([
+
+    ])
+    model.fit(train_X, train_Y, validation_data=(valid_X, valid_Y)
