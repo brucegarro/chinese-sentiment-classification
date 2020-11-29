332909044d608cddebb296660c53542237769625
Bruce Garro <brucegarro@gmail.com>
2020-11-29 14:01:54 -0500

    Allow setup_tf_server to transfer data, embedding matrix, and tokenizer to remote server

--- scripts/setup_tf_server.py
+++ scripts/setup_tf_server.py
@@ -1,11 +1,50 @@
+from os.path import join
 from fabric import task, Connection
+from patchwork.transfers import rsync
 
+from settings.settings import (
+    REN_CEC_PATH,
+    EMBEDDING_MATRIX_NAME,
+    KERAS_TOKENIZER_NAME,
+    EMBEDDING_MATRIX_PATH,
+    KERAS_TOKENIZER_PATH,
+)
+
+
+def transfer_embedding_data(connection):
+    # Add Embedding Data Folder
+    EMBEDDING_REMOTE_PATH = join(connection.config.run.env["REPO_PATH"], "chinese-sentiment-classification-data/embedding")
+    if connection.run("test -d %s" % EMBEDDING_REMOTE_PATH, warn=True).failed:
+        connection.run("mkdir -p %s" % EMBEDDING_REMOTE_PATH)
+        print("Created: %s\n" % EMBEDDING_REMOTE_PATH)
+
+    # Transfer embedding matrix and tokenizer
+    EMBEDDING_MATRIX_REMOTE_PATH = join(EMBEDDING_REMOTE_PATH, EMBEDDING_MATRIX_NAME)
+    KERAS_TOKENIZER_REMOTE_PATH = join(EMBEDDING_REMOTE_PATH, KERAS_TOKENIZER_NAME)
+    for local_filepath, remote_filepath in (
+        (EMBEDDING_MATRIX_PATH, EMBEDDING_MATRIX_REMOTE_PATH),
+        (KERAS_TOKENIZER_PATH, KERAS_TOKENIZER_REMOTE_PATH),
+    ):
+        if connection.run("test -f %s" % remote_filepath, warn=True).failed:
+            rsync(connection, local_filepath, EMBEDDING_REMOTE_PATH)
+            print("Created: %s\n" % remote_filepath)
 
 def setup_directories(connection):
     # Create Repo Path
     if connection.run("test -d $REPO_PATH", warn=True).failed:
         connection.run("mkdir $REPO_PATH")
         print("Created: %s" % connection.config.run.env["REPO_PATH"])
+
+    # Put Ren CEC Repo
+    REN_CEC_REMOTE_PATH = join(connection.config.run.env["REPO_PATH"], "Ren_CECps-Dictionary")
+    if connection.run("test -d %s" % REN_CEC_REMOTE_PATH, warn=True).failed:
+        connection.run("mkdir -p %s" % REN_CEC_REMOTE_PATH)
+        print("Created: %s\n" % REN_CEC_REMOTE_PATH)
+
+        rsync(connection, REN_CEC_PATH, connection.config.run.env["REPO_PATH"])
+        print("Put %s to %s\n" % (REN_CEC_PATH, REN_CEC_REMOTE_PATH))
+
+    transfer_embedding_data(connection)
 
 
 @task()
--- settings/settings.py
+++ settings/settings.py
@@ -1,14 +1,25 @@
+import os
 from os.path import join
 
 from settings.local import *
 
 
-DATA_PATH = join(ALL_REPOS_ROOT, "Ren_CECps-Dictionary/data/CEC_emotionCoprus")
+if os.getenv("REPO_PATH"):
+	ALL_REPOS_ROOT = os.getenv("REPO_PATH")
+else:
+	from settings.local import ALL_REPOS_ROOT
+
+REPO_ROOT = join(ALL_REPOS_ROOT, "chinese-sentiment-classification")
+
+REN_CEC_PATH = join(ALL_REPOS_ROOT, "Ren_CECps-Dictionary")
+DATA_PATH = join(REN_CEC_PATH, "data/CEC_emotionCoprus")
 EMBEDDING_DATA_ROOT = join(ALL_REPOS_ROOT, "chinese-sentiment-classification-data", "embedding")
 RAW_WORD_EMBEDDING_PATH = join(EMBEDDING_DATA_ROOT, "Tencent_AILab_ChineseEmbedding.txt")
 
-EMBEDDING_MATRIX_PATH = join(EMBEDDING_DATA_ROOT, "embedding_matrix_for_vocabulary.pkl")
-KERAS_TOKENIZER_PATH = join(EMBEDDING_DATA_ROOT, "keras_tokenizer_for_vocabulary.pkl")
+EMBEDDING_MATRIX_NAME = "embedding_matrix_for_vocabulary.pkl"
+KERAS_TOKENIZER_NAME = "keras_tokenizer_for_vocabulary.pkl"
+EMBEDDING_MATRIX_PATH = join(EMBEDDING_DATA_ROOT, EMBEDDING_MATRIX_NAME)
+KERAS_TOKENIZER_PATH = join(EMBEDDING_DATA_ROOT, KERAS_TOKENIZER_NAME)
 
 SAVED_MODELS_PATH = join(REPO_ROOT, "saved_models")
 LOGS_PATH = join(REPO_ROOT, "logs")
