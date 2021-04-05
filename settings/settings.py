73bd5068eb4ec0d7a6c95ad30e48f5aeaaf92fd8
Bruce Garro <brucegarro@gmail.com>
2021-04-05 10:42:58 -0400

    Make settings for preprocessing_routines more modular & add pytorch==1.7.1

--- settings/settings.py
+++ settings/settings.py
@@ -3,9 +3,9 @@ from os.path import join
 
 
 if os.getenv("REPO_PATH"):
-	ALL_REPOS_ROOT = os.getenv("REPO_PATH")
+    ALL_REPOS_ROOT = os.getenv("REPO_PATH")
 else:
-	from settings.local import ALL_REPOS_ROOT
+    from settings.local import ALL_REPOS_ROOT
 
 REPO_ROOT = join(ALL_REPOS_ROOT, "chinese-sentiment-classification")
 
@@ -14,13 +14,21 @@ DATA_PATH = join(REN_CEC_PATH, "data/CEC_emotionCoprus")
 
 
 EMBEDDING_DATA_ROOT = os.getenv("EMBEDDING_DATA_ROOT",
-	default=join(ALL_REPOS_ROOT, "chinese-sentiment-classification-data", "embedding"))
+    default=join(ALL_REPOS_ROOT, "chinese-sentiment-classification-data", "embedding"))
 RAW_WORD_EMBEDDING_PATH = join(EMBEDDING_DATA_ROOT, "Tencent_AILab_ChineseEmbedding.txt")
 
-EMBEDDING_MATRIX_NAME = "embedding_matrix_for_vocabulary.pkl"
-KERAS_TOKENIZER_NAME = "keras_tokenizer_for_vocabulary.pkl"
-EMBEDDING_MATRIX_PATH = join(EMBEDDING_DATA_ROOT, EMBEDDING_MATRIX_NAME)
-KERAS_TOKENIZER_PATH = join(EMBEDDING_DATA_ROOT, KERAS_TOKENIZER_NAME)
+class TFProjectSettings(object):
+    EMBEDDING_MATRIX_NAME = "embedding_matrix_for_vocabulary.pkl"
+    TOKENIZER_NAME = "keras_tokenizer_for_vocabulary.pkl"
+    EMBEDDING_MATRIX_PATH = join(EMBEDDING_DATA_ROOT, EMBEDDING_MATRIX_NAME)
+    TOKENIZER_PATH = join(EMBEDDING_DATA_ROOT, TOKENIZER_NAME)
+
+class PTProjectSettings(object):
+    EMBEDDING_MATRIX_NAME = "embedding_matrix_for_vocabulary.pkl"
+    TOKENIZER_NAME = "tokenizer_for_vocabulary.pkl"
+    EMBEDDING_MATRIX_PATH = join(EMBEDDING_DATA_ROOT, "pt_project", EMBEDDING_MATRIX_NAME)
+    TOKENIZER_PATH = join(EMBEDDING_DATA_ROOT, "pt_project", TOKENIZER_NAME)
+
 
 SAVED_MODELS_PATH = join(REPO_ROOT, "saved_models")
 LOGS_PATH = join(REPO_ROOT, "logs")
